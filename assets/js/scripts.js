8f1d471ff6ba774660be4598c59c67d7c202e009
Jakob Class <mikrobi@github.com>
2017-09-30 13:38:12 +0200

    Remove reading time

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,4 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-
-	// Responsive-Nav
 	var nav = responsiveNav(".nav-collapse");
-
-	// Round Reading Time
-    $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
-    });
-
 });
